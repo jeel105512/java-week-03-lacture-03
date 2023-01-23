7cc63d15346eac22121ec75745bb00a72edff3dd
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-01-23 12:21:09 -0800

    added code to controller class

--- src/main/java/com/example/lacture03/WarGameViewController.java
+++ src/main/java/com/example/lacture03/WarGameViewController.java
@@ -1,5 +1,23 @@
 package com.example.lacture03;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class WarGameViewController {
 
+    @FXML
+    private Label faceNameLabel;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Label suitNameLabel;
+    @FXML
+    private Label valueLabel;
+
+    @FXML
+    void nextCard(ActionEvent event) {
+
+    }
 }
